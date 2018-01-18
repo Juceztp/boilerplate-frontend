5b0c6b76646846be8ceec9b0a293ef423d2241b0
Julio Peña <julio.pena@inprovider.cl>
2018-01-18 00:39:32 +0000

    import describe and it

--- test/add-test.js
+++ test/add-test.js
@@ -1,10 +1,11 @@
+import { describe, it } from 'mocha'
 import { expect } from 'chai'
 import { add } from '../src/js/add'
 
-describe ('adding 2 numbers', () => {
+describe('adding 2 numbers', () => {
   it('should return 3 when adding 1 + 2', () => {
-    const sum = add (1, 4)
+    const sum = add(1, 2)
     const resultado = 3
-    expect (sum).to.equal(resultado)
+    expect(sum).to.equal(resultado)
   })
 })
