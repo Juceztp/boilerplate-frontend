26934dbe26a3e6e52072e55e2a736863e52c919e
Julio Pena <jpena@option.cl>
2018-11-19 19:16:29 -0300

    Clean and updated

--- dist/js/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var changeColor = function changeColor() {
-        document.querySelector('.header').addEventListener('click', function () {
-                return document.body.classList.add('backgnd');
-        });
-};
-
-changeColor();
